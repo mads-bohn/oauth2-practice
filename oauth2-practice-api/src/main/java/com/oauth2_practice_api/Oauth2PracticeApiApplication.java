2512f45cdd0bff2c9848a5084e7d7eb8b66e6a48
mads-bohn <mady.bohn@gmail.com>
2025-04-02 22:13:41 -0500

    react and spring boot scaffolding

---
+++ oauth2-practice-api/src/main/java/com/oauth2_practice_api/Oauth2PracticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2_practice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PracticeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2PracticeApiApplication.class, args);
+	}
+
+}
---
+++ oauth2-practice-api/src/test/java/com/oauth2_practice_api/Oauth2PracticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2_practice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PracticeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
