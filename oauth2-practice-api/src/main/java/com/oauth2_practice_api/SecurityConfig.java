c47e622184b619ec4ab449cc9426062a40ea3aac
mads-bohn <mady.bohn@gmail.com>
2025-04-09 15:58:36 -0500

    default security filter chain

---
+++ oauth2-practice-api/src/main/java/com/oauth2_practice_api/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.oauth2_practice_api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable) // disables CSRF
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated()) // requires endpoint authentication
+                .formLogin(Customizer.withDefaults()); // form-based login
+        return http.build();
+    }
+}
