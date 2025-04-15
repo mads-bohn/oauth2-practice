6cdfdc87a72e4958535381eb564a45a1a368d792
mads-bohn <mady.bohn@gmail.com>
2025-04-15 18:36:34 -0500

    onclick handlers

--- oauth2-practice-ui/src/components/Home.jsx
+++ oauth2-practice-ui/src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default function Home() {
+
+    const openGoogle = () => {
+        window.location.href = 'http://localhost:8080/oauth2/authorization/google'
+    }
+
+    const openGithub = () => {
+        window.location.href = 'http://localhost:8080/oauth2/authorization/github'
+    }
+
   return (
     <div>
         <h1>OAuth2 Login</h1>
-        <button>Login with Google</button>
-        <button>Login with Github</button>
+        <button onClick={openGoogle}>Login with Google</button>
+        <button onClick={openGithub}>Login with Github</button>
     </div>
   )
 }
