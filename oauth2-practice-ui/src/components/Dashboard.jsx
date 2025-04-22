b9b3912bb7618c3efccf52b85396d0354fd2e115
mads-bohn <mady.bohn@gmail.com>
2025-04-22 12:42:58 -0500

    user state, effect, and fetch function

--- oauth2-practice-ui/src/components/Dashboard.jsx
+++ oauth2-practice-ui/src/components/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
 
 export default function Dashboard() {
+
+  const [user, setUser] = useState();
+
+  const fetchUser = () => {
+    axios.get('', {withCredentials: true})
+    .then(response => {
+      setUser(response.data);
+    })
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2>Welcome</h2>
+      <p>Name:</p>
+      <p>Email:</p>
+    </div>
   )
 }
