a35344fba15ceb2f447d603d1bc08d8de9811b4a
mads-bohn <mady.bohn@gmail.com>
2025-05-05 13:28:18 -0500

    button icons and styling

--- oauth2-practice-ui/src/components/Home.jsx
+++ oauth2-practice-ui/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 export default function Home() {
 
@@ -10,11 +11,17 @@ export default function Home() {
         window.location.href = 'http://localhost:8080/oauth2/authorization/github'
     }
 
+    const buttonStyle = {
+      fontSize: "20px",
+      padding: "10px"
+    }
+
   return (
     <div>
         <h1>OAuth2 Login</h1>
-        <button onClick={openGoogle}>Login with Google</button>
-        <button onClick={openGithub}>Login with Github</button>
+        <button onClick={openGoogle} style={buttonStyle}><FaGoogle />Login with Google</button>
+        <br />
+        <button onClick={openGithub} style={buttonStyle}><FaGithub />Login with Github</button>
     </div>
   )
 }
